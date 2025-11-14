edf418245b9c0e35d6dd3f1151b8c4175819040a
EisaK5 <eisa@eisakazim.com>
2025-11-14 17:19:59 -0600

    App initialization

---
+++ web/diceRoller/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="app">
+      <h1>Dice Roller</h1>
+      <p>Roll dice below</p>
+    </div>
+  );
+
+}
+
+export default App;
