3e4258bb3f1106b3c56f394b39d092f4a223d5c9
EisaK5 <eisa@eisakazim.com>
2025-11-20 02:23:14 -0600

    major css style changes to rows

--- diceRoller/src/RollDice.jsx
+++ diceRoller/src/RollDice.jsx
@@ -33,162 +33,177 @@ function handleRoll() {
 }
 
 return (
-    <div  className ="RollDice">
-        <label>
-            Attacks:
-            <input
-                type="number"
-                value={attacks}
-                onChange={(e) => setAttacks(Number(e.target.value))}
-            /> 
-        </label>
+    <div  className="RollDice">
+        <h2>Offensive Stats</h2>
+        <div className="RollDiceRow">
+            <div className="RollDiceField">
+                <label>Attacks:</label>
+                <input
+                    type="number"
+                    value={attacks}
+                    onChange={(e) => setAttacks(Number(e.target.value))}
+                /> 
+            </div>
 
-        <label>
-            BS/WS:
-            <input
-                type="number"
-                value={bsWs}
-                onChange={(e) => setSkill(Number(e.target.value))}
-            /> 
-        </label>
+            <div className="RollDiceField">
+                <label>BS/WS:</label>
+                <input
+                    type="number"
+                    value={bsWs}
+                    onChange={(e) => setSkill(Number(e.target.value))}
+                /> 
+            </div>
 
-        <label>
-            Stength:
-            <input
-                type="number"
-                value={strength}
-                onChange={(e) => setStrength(Number(e.target.value))}
-            /> 
-        </label>
+            <div className="RollDiceField">
+                <label>Strength:</label>
+                <input
+                    type="number"
+                    value={strength}
+                    onChange={(e) => setStrength(Number(e.target.value))}
+                /> 
+            </div>
 
-        <label>
-            AP:
-            <input
-                type="number"
-                value={ap}
-                onChange={(e) => setAp(Number(e.target.value))}
-            /> 
-        </label>
+            <div className="RollDiceField">
+                <label>AP:</label>
+                <input
+                    type="number"
+                    value={ap}
+                    onChange={(e) => setAp(Number(e.target.value))}
+                /> 
+            </div>
 
-        <label>
-            Damage:
-            <input
-                type="number"
-                value={damage}
-                onChange={(e) => setDamage(Number(e.target.value))}
-            /> 
-        </label>
+            <div className="RollDiceField">
+                <label>Damage: </label>
+                <input
+                    type="number"
+                    value={damage}
+                    onChange={(e) => setDamage(Number(e.target.value))}
+                />
+            </div>
 
-        <label>
-            sustainedHits:
-            <input
-                type="number"
-                value={sustainedHits}
-                onChange={(e) => setSustainedHits(Number(e.target.value))}
-            />
-        </label>
-        <label>
-            lethalHits:
-            <input
-                type="checkbox"
-                checked={lethalHits}
-                onChange={(e) => setLethalHits(e.target.checked)} 
-            />
-        </label>
-        <label>
-            CriticalHit:
-            <input
-                type="number"
-                check={criticalHit}
-                onChange={(e) => setCriticalHit(Number(e.target.value))}
-            />
-        </label>
-        <label>
-            rerollHits:
-        </label>
-        <select value={rerollHits} onChange={(e) => setRerollHits(e.target.value)}>
-            <option value="">No hit rolls</option>
-            <option value="rollOne">Hit rolls of 1</option>
-            <option value="failedHitRolls">Reroll failed hit rolls</option>
-            <option value="nonHitCritRoll">Reroll non critical rolls</option>
-        </select>
-        <label>
-            CriticalWound:
-            <input
-                type="number"
-                check={criticalWound}
-                onChange={(e) => setCriticalWound(Number(e.target.value))}
-            />
-        </label>
-        <label>
-            devastatingWounds:
-            <input
-                type="checkbox"
-                checked={devastatingWounds}
-                onChange={(e) => setDevastatingWounds(e.target.checked)}
-            />
-        </label>
-        <label>
-            rerollWounds:
-        </label>
-        <select value={rerollWounds} onChange={(e) => setRerollWounds(e.target.value)}>
-            <option value="">No wound rolls</option>
-            <option value="rollOne">Wound rolls of 1</option>
-            <option value="failedWoundRolls">Reroll failed wound rolls</option>
-            <option value="nonWoundCritRoll">Reroll non critical rolls</option>
-        </select>
+            <div className="RollDiceField">
+                <label>Sustained Hits:</label>
+                <input
+                    type="number"
+                    value={sustainedHits}
+                    onChange={(e) => setSustainedHits(Number(e.target.value))}
+                />
+            </div>
+        </div>
+
+        <div className="RollDiceRow">
+            <div className="RollDiceField">
+                <label>Lethal Hits:</label>
+                <input
+                    type="checkbox"
+                    checked={lethalHits}
+                    onChange={(e) => setLethalHits(e.target.checked)} 
+                />
+            </div>
+
+            <div className="RollDiceField">
+                <label>Critical Hit:</label>
+                <input
+                    type="number"
+                    check={criticalHit}
+                    onChange={(e) => setCriticalHit(Number(e.target.value))}
+                />
+            </div>
+
+            <div className="RollDiceField">
+                <label>rerollHits:</label>
+                <select value={rerollHits} onChange={(e) => setRerollHits(e.target.value)}>
+                    <option value="">No hit rolls</option>
+                    <option value="rollOne">Hit rolls of 1</option>
+                    <option value="failedHitRolls">Reroll failed hit rolls</option>
+                    <option value="nonHitCritRoll">Reroll non critical rolls</option>
+                </select>
+            </div>
         
-        <label>
-            Toughness:
-            <input
-                type="number"
-                value={toughness}
-                onChange={(e) => setToughness(Number(e.target.value))}
-            /> 
-        </label>
+            <div className="RollDiceField">
+                <label>Critical Wound:</label>
+                <input
+                    type="number"
+                    check={criticalWound}
+                    onChange={(e) => setCriticalWound(Number(e.target.value))}
+                />
+            </div>
 
-        <label>
-            Save:
-            <input
-                type="number"
-                value={save}
-                onChange={(e) => setSave(Number(e.target.value))}
-            /> 
-        </label>
-        <label>
-            Invunerable Save:
-            <input
-                type="number"
-                value={invuln}
-                onChange={(e) => setInvuln(Number(e.target.value))}
-            /> 
-        </label>
-        <label>
-            Feel no pain:
-            <input
-                type="number"
-                value={fnp}
-                onChange={(e) => setFnp(Number(e.target.value))}
-            />
-        </label>
-        <label>
-            Wounds:
-            <input
-                type="number"
-                value={wounds}
-                onChange={(e) => setWounds(Number(e.target.value))}
-            /> 
-        </label>
+            <div className="RollDiceField">
+                <label>Devastating Wounds:</label>
+                <input
+                    type="checkbox"
+                    checked={devastatingWounds}
+                    onChange={(e) => setDevastatingWounds(e.target.checked)}
+                />
+            </div>
 
-        <label>
-            Sample size:
-            <input
-                type="number"
-                value={N}
-                onChange={(e) => setN(Number(e.target.value))}
-            /> 
-        </label>
+            <div className="RollDiceField">
+                <label>Reroll Wounds:</label>
+                <select value={rerollWounds} onChange={(e) => setRerollWounds(e.target.value)}>
+                    <option value="">No wound rolls</option>
+                    <option value="rollOne">Wound rolls of 1</option>
+                    <option value="failedWoundRolls">Reroll failed wound rolls</option>
+                    <option value="nonWoundCritRoll">Reroll non critical rolls</option>
+                </select>
+            </div>
+        </div>
+        <h2>Defensive Stats</h2>
+        <div className="RollDiceRow">
+            <div className="RollDiceField">
+                <label>Toughness:</label>
+                <input
+                    type="number"
+                    value={toughness}
+                    onChange={(e) => setToughness(Number(e.target.value))}
+                />
+            </div>
 
+            <div className="RollDiceField">
+                <label>Save:</label>
+                <input
+                    type="number"
+                    value={save}
+                    onChange={(e) => setSave(Number(e.target.value))}
+                /> 
+            </div>
+
+            <div className="RollDiceField">
+                <label>Invunerable Save:</label>
+                <input
+                    type="number"
+                    value={invuln}
+                    onChange={(e) => setInvuln(Number(e.target.value))}
+                /> 
+            </div>
+
+            <div className="RollDiceField">
+                <label>Feel no pain:</label>
+                <input
+                    type="number"
+                    value={fnp}
+                    onChange={(e) => setFnp(Number(e.target.value))}
+                />
+            </div>
+
+            <div className="RollDiceField">
+                <label>Wounds:</label>
+                <input
+                    type="number"
+                    value={wounds}
+                    onChange={(e) => setWounds(Number(e.target.value))}
+                /> 
+            </div>
+
+            <div className="RollDiceField">
+                <label>Sample size:</label>
+                <input
+                    type="number"
+                    value={N}
+                    onChange={(e) => setN(Number(e.target.value))}
+                /> 
+            </div>
+        </div>
         <button onClick={handleRoll}>Roll</button>
         {result && (
             <div>
