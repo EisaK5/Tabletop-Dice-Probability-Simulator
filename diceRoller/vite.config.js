30ae6b7d321a5f28ca68e4825f4c1cc176217b80
EisaK5 <eisa@eisakazim.com>
2025-12-18 16:22:01 -0600

    connected to repo for deployment

--- diceRoller/vite.config.js
+++ diceRoller/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/40k-Dice-Roller",
+  base: "/Tabletop-Dice-Probability-Simulator",
   plugins: [react()],
 })
